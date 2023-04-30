7f4eb4fb01a3ece2752a477f9b39c9eab57a835f
Teshan Jayakody <it21345296@my.sliit.lk>
2023-04-30 11:13:04 +0530

    View profile!

---
+++ app/src/main/java/com/example/pawningsystem/ViewProfile.kt
@@ -0,0 +1,11 @@
+package com.example.pawningsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_profile)
+    }
+}
