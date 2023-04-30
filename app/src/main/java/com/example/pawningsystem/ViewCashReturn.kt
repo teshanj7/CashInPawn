465a3faba07a29477d4b3f9edf5f259391a5f355
Teshan Jayakody <it21345296@my.sliit.lk>
2023-04-30 20:35:06 +0530

    Cash return view

--- app/src/main/java/com/example/pawningsystem/ViewCashReturn.kt
+++ app/src/main/java/com/example/pawningsystem/ViewCashReturn.kt
@@ -3,9 +3,9 @@ package com.example.pawningsystem
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class UpdateCashReturn : AppCompatActivity() {
+class ViewCashReturn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_update_cash_return)
+        setContentView(R.layout.activity_view_cash_return)
     }
 }
