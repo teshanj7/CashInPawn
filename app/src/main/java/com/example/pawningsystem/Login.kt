bbcfd69d1300108afa83bf2d2a305601a29c410f
Teshan Jayakody <it21345296@my.sliit.lk>
2023-05-02 22:51:20 +0530

    Linking initially

--- app/src/main/java/com/example/pawningsystem/Login.kt
+++ app/src/main/java/com/example/pawningsystem/Login.kt
@@ -1,11 +1,29 @@
 package com.example.pawningsystem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginBtn = findViewById<Button>(R.id.btn_login)
+        val signUpLink = findViewById<TextView>(R.id.signup_login)
+
+        loginBtn.setOnClickListener {
+            val intent = Intent(this, ActivityDashBoard::class.java)
+            Toast.makeText(this, "Successfully logged in!", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
+        }
+
+        signUpLink.setOnClickListener{
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
     }
 }
