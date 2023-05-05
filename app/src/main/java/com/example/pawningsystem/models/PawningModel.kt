22c9756283c0e070e572b4b7baf80def047e4158
Teshan Jayakody <it21345296@my.sliit.lk>
2023-05-05 19:38:42 +0530

    commit done linking bihesha n Teshan

--- app/src/main/java/com/example/pawningsystem/models/PawningModel.kt
+++ app/src/main/java/com/example/pawningsystem/models/PawningModel.kt
@@ -3,7 +3,7 @@ package com.example.pawningsystem.models
 data class PawningModel(
     var psId: String? = null,
     var psName: String? = null,
-    var psNIC: String? = null,
+    var email: String? = null,
     var psTeleNo: String? = null,
     var psBankNo: String? = null,
     var psAddress: String? = null,
