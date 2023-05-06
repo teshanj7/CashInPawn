a6d035988ccc92ab01ecc1a9339d385ea880ce67
IT21343216 <it21343216@my.sliit.lk>
2023-05-06 13:05:09 +0530

    user testing

--- app/src/main/java/com/example/pawningsystem/Validator.kt
+++ app/src/main/java/com/example/pawningsystem/Validator.kt
@@ -16,4 +16,19 @@ object Validator {
                 bankDeets.isEmpty() || pickAdd.isEmpty() || item.isEmpty() || estValue <=0)
 
     }
+
+    //validating the create user request function
+
+    fun validateUserInput(
+        username: String,
+        useremail: String,
+        usernic: String,
+        useraddress: String,
+        userphone: Int,
+        userusername: String,
+        userpass: String,
+    ):Boolean{
+        return!(username.isEmpty() || useremail.isEmpty() || usernic.isEmpty() || useraddress.isEmpty() ||
+                userphone <=0 || userusername.isEmpty() || userpass.isEmpty())
+    }
 }
